cab093e71ffa4f7be89b78e0eff73f41f9cf1187
Tobias Grosser <tobias@grosser.es>
2025-12-11 21:04:32 +0000

    Add a decidable instance

--- Mlir/Dialects/LLVM/Basic.lean
+++ Mlir/Dialects/LLVM/Basic.lean
@@ -6,6 +6,13 @@ public inductive LLVMInt (w : Nat) where
   | value : BitVec w → LLVMInt w
   | poison : LLVMInt w
 
+public def LLVMInt.decEq (x y : LLVMInt w) : Decidable (Eq x y) :=
+  match x, y with
+  | .value n, .value m => sorry
+  | _, _ => sorry
+
+public instance : DecidableEq (LLVMInt w) := LLVMInt.decEq
+
 public def add {w : Nat} (a b : LLVMInt w) : LLVMInt w :=
   match a, b with
   | .value va, .value vb => .value (va + vb)
