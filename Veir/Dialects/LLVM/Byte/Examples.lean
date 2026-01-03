a43fae383f31aa942ae10bd51c7e729f00eb22ed
Tobias Grosser <tobias@grosser.es>
2026-01-03 20:30:50 +0000

    chore: make bv_decide work on byte type
    Additionally add a hypothesis that the value bits are zero if the udef bit is set.w

---
+++ Veir/Dialects/LLVM/Byte/Examples.lean
@@ -0,0 +1,19 @@
+module
+
+import Std.Tactic.BVDecide
+public import Veir.Dialects.LLVM.Int.Basic
+import all Veir.Dialects.LLVM.Byte.Basic
+import all Veir.Dialects.LLVM.Byte.Lemmas
+
+namespace Veir.Dialects.LLVM.Byte
+
+example (w : Nat) (x y : Byte w) : x ||| y = y ||| x := by
+  simp only [HOr.hOr, OrOp.or, or]
+  simp [BitVec.or_comm (x.poison) (y.poison), BitVec.or_comm (x.val) (y.val)]
+
+example (x y : Byte 8) : x ||| y = y ||| x := by
+  rw [ext_iff]
+  simp only [HOr.hOr, OrOp.or, or]
+  bv_decide
+
+end Veir.Dialects.LLVM.Byte
---
+++ Veir/Dialects/LLVM/Byte/Lemmas.lean
@@ -0,0 +1,26 @@
+module
+
+public import Veir.Dialects.LLVM.Int.Basic
+import all Veir.Dialects.LLVM.Byte.Basic
+
+namespace Veir.Dialects.LLVM.Byte
+
+open Veir.Dialects.LLVM.Int
+
+theorem toInt_fromInt {w : Nat} (x : Int w) (h : 0 < w) : (Byte.fromInt x).toInt = x := by
+  simp only [Byte.toInt, fromInt]
+  cases x
+  · simp
+  · have := Nat.two_pow_pred_lt_two_pow h
+    have := Nat.two_pow_pos (w-1)
+    rw [ite_eq_right_iff, BitVec.toNat_eq]
+    simp
+    omega
+
+theorem ext_iff {w : Nat} (x y : Byte w) :
+    x = y ↔ (x.val = y.val ∧ x.poison = y.poison) := by
+  constructor
+  <;> rw [Byte.mk.injEq]
+  <;> simp
+
+end Veir.Dialects.LLVM.Byte
