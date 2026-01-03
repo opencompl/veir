a52e976088ae66f8d171b025b5637e545b652954
Tobias Grosser <tobias@grosser.es>
2026-01-03 22:12:28 +0000

    complete and/or/xor theorems

--- Veir/Dialects/LLVM/Byte/Lemmas.lean
+++ Veir/Dialects/LLVM/Byte/Lemmas.lean
@@ -7,19 +7,6 @@ namespace Veir.Dialects.LLVM.Byte
 
 open Veir.Dialects.LLVM.Int
 
-/- # or -/
-
-theorem or_eq {w : Nat} (x y : Byte w) :
-    (x ||| y) =
-    let poison := x.poison ||| y.poison
-    ⟨(x.val ||| y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩ := by
-  rfl
-
-@[simp]
-theorem val_or {w : Nat} (x y : Byte w) :
-    (x ||| y).val = (x.val ||| y.val) &&& ~~~(x.poison ||| y.poison) := by
-  simp [or_eq]
-
 theorem toInt_fromInt {w : Nat} (x : Int w) (h : 0 < w) : (Byte.fromInt x).toInt = x := by
   simp only [Byte.toInt, fromInt]
   cases x
@@ -35,5 +22,53 @@ theorem ext_iff {w : Nat} (x y : Byte w) :
   constructor
   <;> rw [Byte.mk.injEq]
   <;> simp
+
+/- # and -/
+
+theorem and_eq {w : Nat} (x y : Byte w) :
+    (x &&& y) =
+    let poison := x.poison ||| y.poison
+    ⟨(x.val &&& y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩ := by
+  rfl
+
+theorem and_comm {w : Nat} (x y : Byte w) :
+    x &&& y = y &&& x := by
+  simp only [and_eq, BitVec.and_comm, BitVec.or_comm]
+
+theorem val_and {w : Nat} (x y : Byte w) :
+    (x&&& y).val = (x.val &&& y.val) &&& ~~~(x.poison ||| y.poison) := by
+  simp [and_eq]
+
+/- # or -/
+
+theorem or_eq {w : Nat} (x y : Byte w) :
+    (x ||| y) =
+    let poison := x.poison ||| y.poison
+    ⟨(x.val ||| y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩ := by
+  rfl
+
+theorem or_comm {w : Nat} (x y : Byte w) :
+    x ||| y = y ||| x := by
+  simp only [or_eq, BitVec.or_comm]
+
+theorem val_or {w : Nat} (x y : Byte w) :
+    (x ||| y).val = (x.val ||| y.val) &&& ~~~(x.poison ||| y.poison) := by
+  simp [or_eq]
+
+/- # xor -/
+
+theorem xor_eq {w : Nat} (x y : Byte w) :
+    (x ^^^ y) =
+    let poison := x.poison ||| y.poison
+    ⟨(x.val ^^^ y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩ := by
+  rfl
+
+theorem xor_comm {w : Nat} (x y : Byte w) :
+    x ^^^ y = y ^^^ x := by
+  simp only [xor_eq, BitVec.xor_comm, BitVec.or_comm]
+
+theorem val_xor {w : Nat} (x y : Byte w) :
+    (x ^^^ y).val = (x.val ^^^ y.val) &&& ~~~(x.poison ||| y.poison) := by
+  simp [xor_eq]
 
 end Veir.Dialects.LLVM.Byte
