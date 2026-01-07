7da316d224d74c35f02cabbe93b00793fbf2b949
Tobias Grosser <tobias@grosser.es>
2026-01-07 15:00:32 +0000

    make bv_decide work on byte type (#81)
    Additionally add a hypothesis that the value bits are zero if the undef
    bit is set.

--- Veir/Dialects/LLVM/Byte/Basic.lean
+++ Veir/Dialects/LLVM/Byte/Basic.lean
@@ -18,15 +18,27 @@ structure Byte (w : Nat) where
   val : BitVec w
   /-- A per-bit poison mask of width `w`. -/
   poison : BitVec w
+  /-- Invariant: if a bit is poison, the corresponding bit in `val` is zero. -/
+  h : val &&& poison = 0
+deriving DecidableEq
 
-def xor {w : Nat} (x y : Byte w) : Byte w :=
-  ⟨x.val ^^^ y.val, x.poison ||| y.poison⟩
+def and (x y : Byte w) : Byte w :=
+  let poison := x.poison ||| y.poison
+  ⟨(x.val &&& y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩
 
-def or {w : Nat} (x y : Byte w) : Byte w :=
-  ⟨x.val ||| y.val, x.poison ||| y.poison⟩
+instance {w : Nat} : AndOp (Byte w) := ⟨and⟩
 
-def and {w : Nat} (x y : Byte w) : Byte w :=
-  ⟨x.val &&& y.val, x.poison ||| y.poison⟩
+def or (x y : Byte w) : Byte w :=
+  let poison := x.poison ||| y.poison
+  ⟨(x.val ||| y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩
+
+instance {w : Nat} : OrOp (Byte w) := ⟨or⟩
+
+def xor (x y : Byte w) : Byte w :=
+  let poison := x.poison ||| y.poison
+  ⟨(x.val ^^^ y.val) &&& ~~~poison, poison, by simp [BitVec.and_assoc]⟩
+
+instance {w : Nat} : XorOp (Byte w) := ⟨xor⟩
 
 open Int
 
@@ -34,7 +46,7 @@ open Int
   A byte where no bit is poison is equal to an integer value.
   If any bit is poison, the integer type is also poison.
 -/
-def toInt {w : Nat} (x : Byte w) : Int w :=
+def Byte.toInt {w : Nat} (x : Byte w) : Int w :=
   if x.poison = 0 then
     .val x.val
   else
@@ -42,10 +54,9 @@ def toInt {w : Nat} (x : Byte w) : Int w :=
 
 def fromInt {w : Nat} (x : Int w) : Byte w :=
   if let .val x := x then
-    ⟨x, 0⟩
+    ⟨x, 0, by simp⟩
   else
-    ⟨0, BitVec.allOnes w⟩
-
+    ⟨0, BitVec.allOnes w, by simp⟩
 end
 
 end Byte
---
+++ Veir/Dialects/LLVM/Byte/Examples.lean
@@ -0,0 +1,17 @@
+module
+
+import Std.Tactic.BVDecide
+public import Veir.Dialects.LLVM.Int.Basic
+import all Veir.Dialects.LLVM.Byte.Basic
+import all Veir.Dialects.LLVM.Byte.Lemmas
+
+namespace Veir.Dialects.LLVM.Byte
+
+example (w : Nat) (x y : Byte w) : x ||| y = y ||| x := by
+  simp only [or_eq]
+  simp [BitVec.or_comm (x.poison) (y.poison), BitVec.or_comm (x.val) (y.val)]
+
+example (x y : Byte 8) : x ||| y = y ||| x := by
+  bv_decide
+
+end Veir.Dialects.LLVM.Byte
