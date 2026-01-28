779d6a7d937d69fd4c2a80ab3471777b59a70947
George Rennie <georgerennie@gmail.com>
2026-01-28 00:07:13 +0000

    Make parseOptionOp linear in context (#152)

--- Veir/Parser/MlirParser.lean
+++ Veir/Parser/MlirParser.lean
@@ -331,7 +331,12 @@ partial def parseOptionalOp (ip : Option InsertPoint) : MlirParserM (Option Oper
 
   /- Create the operation. -/
   let opId := operationNameToOpId opName
-  let some (ctx, op) := Rewriter.createOp (← getContext) opId outputTypes operands blockOperands regions 0 ip (by sorry) (by sorry) (by sorry) (by sorry) (by sorry)
+
+  /- Set context in monad to default to preserve linearity whilst modifying -/
+  let ctx ← getContext
+  setContext Inhabited.default
+
+  let some (ctx, op) := Rewriter.createOp ctx opId outputTypes operands blockOperands regions 0 ip (by sorry) (by sorry) (by sorry) (by sorry) (by sorry)
       | throw "internal error: failed to create operation"
 
   /- Update the parser context. -/
