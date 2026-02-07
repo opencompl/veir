7faebbcdbb706da40f50d7a89e5309962035bcae
math-fehr <mathieu.fehr@gmail.com>
2026-02-07 23:47:09 +0000

    Fix tests

--- UnitTest/AttrParser.lean
+++ UnitTest/AttrParser.lean
@@ -60,7 +60,7 @@ macro "#assert " e:term : command =>
 
 #assert expectError "\"" "expected '\"' in string literal"
 
-#assert expectSuccess "i32" IntegerType.mk 32
-#assert expectSuccess "i0" IntegerType.mk 0
+#assert expectSuccess "i32" (IntegerType.mk 32)
+#assert expectSuccess "i0" (IntegerType.mk 0)
 #assert expectMissing "foo"
 #assert expectMissing "i0x4"
