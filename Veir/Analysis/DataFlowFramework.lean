68a8910077499e83c66ba4cb6d409fa47ab13857
Axel Sorenson <AxelPSorenson@gmail.com>
2026-02-18 20:17:39 -0700

    DataFlow Framework Rework (WIP)

--- Veir/Analysis/DataFlowFramework.lean
+++ Veir/Analysis/DataFlowFramework.lean
@@ -19,39 +19,48 @@ inductive LatticeAnchor
   | ValuePtr 
 deriving BEq, Hashable
 
-class DataFlowAnalysis (ctx : Type) where
-  impl : Type
-  init : impl -> OperationPtr -> ctx -> ctx
+structure DataFlowAnalysis where
+  ctx : Type
+  init : OperationPtr -> ctx -> ctx
 
 -- ================================== WorkList =================================== -- 
 def WorkItem := ProgramPoint × DataFlowAnalysis
 def WorkList := Queue WorkItem
 -- =============================================================================== -- 
 
+structure DependentPtr where
+  id: Nat
+  deriving BEq, Hashable
+
+structure DataFlowContext where
+  workList : Queue (ProgramPoint × DataFlowAnalysis) 
+  dependents : HashMap DependentPtr WorkItem
+
 -- ================================ AnalysisState ================================ -- 
 -- Lattice Elements are stored in structures that implement the `Update` typeclass.
 -- These structures contain a `BaseAnalysisState` along with anything else it needs.
 -- `AnalysisState` is used to allow for dynamic dispatch (runtime polymorphism).
 class Update (α : Type) where
-  onUpdate : α -> WorkList -> WorkList 
+  onUpdate : α -> DataFlowContext -> DataFlowContext 
 
 structure BaseAnalysisState where
   anchor : LatticeAnchor
-  dependents : Array WorkItem
+  dependents : Array DependentPtr 
 
 instance : Update BaseAnalysisState where
-  onUpdate (state : BaseAnalysisState) (workList : WorkList) : WorkList := Id.run do
-    let mut workList := workList
-    for workItem in state.dependents do
-      workList := workList.enqueue workItem
-    workList 
+  onUpdate (state : BaseAnalysisState) (ctx : DataFlowContext) : DataFlowContext := Id.run do
+    let mut ctx := ctx 
+    for ptr in state.dependents do
+      let workItem := sorry --(ctx.dependents.get! ptr) 
+      ctx := {ctx with workList := ctx.workList.enqueue workItem}
+    ctx 
 
 structure AnalysisState where
   impl : Type
   inst : Update impl
 -- =============================================================================== -- 
 
-def LatticeContext := HashMap LatticeAnchor AnalysisState 
+def Lattice := HashMap LatticeAnchor AnalysisState 
 
 def fixpointSolve (top: OperationPtr) (analyses : Array DataFlowAnalysis) : LatticeContext :=
   let latticeCtx : LatticeContext := sorry
