0fdb6ea24d90708a0ec2f40931690acda5e24f57
Alexander Viand <alexander.viand@gmail.com>
2026-02-19 14:47:17 +0000

    add missing mod_arith.barret_reduce properties

--- Veir/Properties.lean
+++ Veir/Properties.lean
@@ -42,6 +42,23 @@ def ModArithConstantProperties.fromAttrDict (attrDict : Std.HashMap ByteArray At
   return { value := intAttr }
 
 /--
+  Properties of the `mod_arith.barrett_reduce` operation.
+-/
+structure ModArithBarrettReduceProperties where
+  modulus : IntegerAttr
+deriving Inhabited, Repr, Hashable
+
+def ModArithBarrettReduceProperties.fromAttrDict (attrDict : Std.HashMap ByteArray Attribute) :
+    Except String ModArithBarrettReduceProperties := do
+  if attrDict.size > 1 then
+    throw s!"mod_arith.barrett_reduce: expected only 'modulus' property, but got {attrDict.size} properties"
+  let some attr := attrDict["modulus".toUTF8]?
+    | throw "mod_arith.barrett_reduce: missing 'modulus' property"
+  let .integerAttr intAttr := attr
+    | throw s!"mod_arith.barrett_reduce: expected 'modulus' to be an integer attribute, but got {attr}"
+  return { modulus := intAttr }
+
+/--
   A type family that maps an operation code to the type of its properties.
   For operations that do not have any properties, the type is `Unit`.
 -/
@@ -50,6 +67,7 @@ def propertiesOf (opCode : OpCode) : Type :=
 match opCode with
 | .arith_constant => ArithConstantProperties
 | .mod_arith_constant => ModArithConstantProperties
+| .mod_arith_barrett_reduce => ModArithBarrettReduceProperties
 | _ => Unit
 
 instance (opCode : OpCode) : Inhabited (propertiesOf opCode) := by
@@ -69,6 +87,7 @@ def Properties.fromAttrDict (opCode : OpCode) (attrDict : Std.HashMap ByteArray 
   cases opCode
   case arith_constant => exact (ArithConstantProperties.fromAttrDict attrDict)
   case mod_arith_constant => exact (ModArithConstantProperties.fromAttrDict attrDict)
+  case mod_arith_barrett_reduce => exact (ModArithBarrettReduceProperties.fromAttrDict attrDict)
   all_goals exact (Except.ok ())
 
 /--
@@ -81,6 +100,8 @@ def Properties.toAttrDict (opCode : OpCode) (props : propertiesOf opCode) :
     (Std.HashMap.emptyWithCapacity 2).insert "value".toUTF8 (Attribute.integerAttr props.value)
   | .mod_arith_constant =>
     (Std.HashMap.emptyWithCapacity 2).insert "value".toUTF8 (Attribute.integerAttr props.value)
+  | .mod_arith_barrett_reduce =>
+    (Std.HashMap.emptyWithCapacity 2).insert "modulus".toUTF8 (Attribute.integerAttr props.modulus)
   | _ =>
     Std.HashMap.emptyWithCapacity 0
 
