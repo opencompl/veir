46d267c5af0007f2098875886825b1c2b412eb7e
Alexander Viand <alexander.viand@gmail.com>
2026-02-19 16:21:08 +0000

    test updates

--- UnitTest/ModArith.lean
+++ UnitTest/ModArith.lean
@@ -7,7 +7,7 @@ open Veir
 open Veir.Parser
 
 set_option warn.sorry false in
-def testBarretReducePattern (s : String) : IO Unit := do
+def testPattern (pattern : RewritePattern) (s : String) : IO Unit := do
   let some (ctx, _) := IRContext.create
     | throw (IO.userError "internal error: failed to create IR context")
   let parser ← match ParserState.fromInput s.toByteArray with
@@ -18,9 +18,21 @@ def testBarretReducePattern (s : String) : IO Unit := do
     | .ok parsed => pure parsed
     | .error err => throw (IO.userError err)
   let some rewritten := RewritePattern.applyInContext
-      Veir.Transforms.ModArith.barretReduceRewriterPattern state.ctx sorry
+      pattern state.ctx sorry
     | throw (IO.userError "rewrite failed")
   Printer.printOperation rewritten topOp
+
+set_option warn.sorry false in
+def testBarretReducePattern (s : String) : IO Unit := do
+  testPattern Veir.Transforms.ModArith.barretReduceRewriterPattern s
+
+set_option warn.sorry false in
+def testSubIfGePattern (s : String) : IO Unit := do
+  testPattern Veir.Transforms.ModArith.subIfGeRewriterPattern s
+
+set_option warn.sorry false in
+def testReducePattern (s : String) : IO Unit := do
+  testPattern Veir.Transforms.ModArith.reduceRewriterPattern s
 
 /--
   info: "builtin.module"() ({
@@ -46,5 +58,53 @@ def testBarretReducePattern (s : String) : IO Unit := do
   ^bb0(%arg0: i10):
     %0 = "mod_arith.barrett_reduce"(%arg0) <{modulus = 17 : i64}> : (i10) -> i10
     "func.return"(%0) : (i10) -> ()
+  }) : () -> ()
+}) : () -> ()"#
+
+/--
+  info: "builtin.module"() ({
+  ^4():
+    "func.func"() ({
+      ^6(%arg6_0 : i10, %arg6_1 : i10):
+        %11 = "arith.subi"(%arg6_0, %arg6_1) : (i10, i10) -> i10
+        %12 = "arith.cmpi"(%arg6_0, %arg6_1) : (i10, i10) -> i1
+        %13 = "arith.select"(%12, %11, %arg6_0) : (i1, i10, i10) -> i10
+        "func.return"(%13) : (i10) -> ()
+    }) : () -> ()
+}) : () -> ()
+-/
+#guard_msgs in
+#eval! testSubIfGePattern r#""builtin.module"() ({
+  "func.func"() <{function_type = (i10, i10) -> i10, sym_name = "test_lower_subifge_int"}> ({
+  ^bb0(%arg0: i10, %arg1: i10):
+    %0 = "mod_arith.subifge"(%arg0, %arg1) : (i10, i10) -> i10
+    "func.return"(%0) : (i10) -> ()
+  }) : () -> ()
+}) : () -> ()"#
+
+/--
+  info: "builtin.module"() ({
+  ^4():
+    "func.func"() ({
+      ^6(%arg6_0 : i10):
+        %7 = "mod_arith.encapsulate"(%arg6_0) : (i10) -> !mod_arith.int<17 : i10>
+        %13 = "mod_arith.extract"(%7) : (!mod_arith.int<17 : i10>) -> i10
+        %14 = "mod_arith.barrett_reduce"(%13) <{ "modulus" = 17 : i10 }> : (i10) -> i10
+        %15 = "arith.constant"() <{ "value" = 17 : i10 }> : () -> i10
+        %16 = "mod_arith.subifge"(%14, %15) : (i10, i10) -> i10
+        %17 = "mod_arith.encapsulate"(%16) : (i10) -> !mod_arith.int<17 : i10>
+        %9 = "mod_arith.extract"(%17) : (!mod_arith.int<17 : i10>) -> i10
+        "func.return"(%9) : (i10) -> ()
+    }) : () -> ()
+}) : () -> ()
+-/
+#guard_msgs in
+#eval! testReducePattern r#""builtin.module"() ({
+  "func.func"() <{function_type = (i10) -> i10, sym_name = "test_lower_reduce_int"}> ({
+  ^bb0(%arg0: i10):
+    %0 = "mod_arith.encapsulate"(%arg0) : (i10) -> !mod_arith.int<17 : i10>
+    %1 = "mod_arith.reduce"(%0) : (!mod_arith.int<17 : i10>) -> !mod_arith.int<17 : i10>
+    %2 = "mod_arith.extract"(%1) : (!mod_arith.int<17 : i10>) -> i10
+    "func.return"(%2) : (i10) -> ()
   }) : () -> ()
 }) : () -> ()"#
