5f5992e609121590865f181d7da2a43b80d3f259
Alexander Viand <alexander.viand@gmail.com>
2026-03-10 16:33:03 +0800

    add mod_arith-to-arith lowering pass

---
+++ Veir/PatternRewriter/Lemmas.lean
@@ -0,0 +1,22 @@
+import Veir.PatternRewriter.Basic
+import Veir.Interpreter
+
+namespace Veir.PatternRewriter
+
+def interpretOpList' (ctx : IRContext OpCode) (list : List OperationPtr) (state : InterpreterState)
+    (opInBounds : ∀ op ∈ list, op.InBounds ctx := by grind)
+    : Option InterpreterState := do
+  match list with
+  | [] => return state
+  | op :: rest =>
+    let (state, action) ← interpretOp ctx op state
+    match action with
+    | .continue => interpretOpList' ctx rest state
+    | .return results => none
+
+def LocalRewritePattern.PreservesSemantics (pattern : LocalRewritePattern OpCode) : Prop :=
+  ∀ (ctx : IRContext OpCode) (op : OperationPtr) (opIn : op.InBounds ctx) newCtx newOps newValue,
+    pattern ctx op = some (newCtx, some (newOps, #[newValue])) →
+    ∀ state newState, interpretOp ctx op state = some (newState, .continue) →
+    ∃ newState', interpretOpList' ctx newOps.toList state (by sorry) = some newState' ∧
+    newState'.variables[newValue]! = newState.variables[ValuePtr.opResult (op.getResult 0)]!
