e4215f69ea23da843a16151d1c4ac104b9c3f365
luisacicolini <lc985@cam.ac.uk>
2026-04-01 12:57:55 +0100

    chore: more meaningful example

--- Test/Interpreter/RISCV/unrealized_conversion_cast.mlir
+++ Test/Interpreter/RISCV/unrealized_conversion_cast.mlir
@@ -2,10 +2,10 @@
 
 "builtin.module"() ({
   %x = "riscv.li"() <{ "value" = 3 : i64 }> : () -> !reg
-  %u = "builtin.unrealized_conversion_cast"(%x) : (!reg) -> i64
-  %v = "builtin.unrealized_conversion_cast"(%u) : (i64) -> !reg
-  "func.return"(%u, %v) : (i64, !reg) -> ()
+  %u = "builtin.unrealized_conversion_cast"(%x) : (!reg) -> i8
+  %v = "builtin.unrealized_conversion_cast"(%u) : (i8) -> !reg
+  "func.return"(%u, %v) : (i8, !reg) -> ()
 }) : () -> ()
 
-// CHECK: Program output: #[0x0000000000000003#64, 0x0000000000000003#64]
+// CHECK: Program output: #[0x03#8, 0x0000000000000003#64]
 
--- Veir/Passes/InstructionSelection/RISCV64.lean
+++ Veir/Passes/InstructionSelection/RISCV64.lean
@@ -18,9 +18,11 @@ def constant (rewriter: PatternRewriter OpCode) (op: OperationPtr) :
   let some const := matchConstantOp op rewriter.ctx
       | return rewriter
   if const.type.bitwidth ≠ 64 then return rewriter
-  let type := ((op.getResult 0).get! rewriter.ctx).type
-  let .integerType type' := type.val | rewriter
-  if type'.bitwidth ≠ 64 then return rewriter
+  let ret := ((op.getResult 0).get! rewriter.ctx).type
+  let .integerType type := ret.val | rewriter
+  /- first cast `i64` to register -/
+  let (rewriter, newOp) ← rewriter.createOp .builtin_unrealized_conversion_cast #[type] #[]
+      #[] #[] {value := const} (some $ .before op) (by simp) (by simp) (by simp) sorry
   let (rewriter, newOp) ← rewriter.createOp .riscv_li #[type] #[]
       #[] #[] {value := const} (some $ .before op) (by simp) (by simp) (by simp) sorry
   rewriter.replaceOp op newOp sorry sorry sorry
