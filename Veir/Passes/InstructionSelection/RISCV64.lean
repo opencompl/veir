3da3a3db5f1d2d0c52de1b27c2a176582e09ae2c
luisacicolini <lc985@cam.ac.uk>
2026-04-02 08:41:46 +0100

    remove comments

--- Veir/Passes/InstructionSelection/RISCV64.lean
+++ Veir/Passes/InstructionSelection/RISCV64.lean
@@ -21,8 +21,6 @@ def constant (rewriter: PatternRewriter OpCode) (op: OperationPtr) :
   let (rewriter, newOp) ← rewriter.createOp .riscv_li #[RegisterType.mk] #[]
       #[] #[] {value := const} (some $ .before op) (by simp) (by simp) (by simp) sorry
   let type := ((op.getResult 0).get! rewriter.ctx).type
-  -- let .integerType type' := type.val | rewriter
-  -- if type'.bitwidth ≠ 64 then return rewriter
   let (rewriter, castOp) ← rewriter.createOp .builtin_unrealized_conversion_cast #[type] #[newOp.getResult 0]
       #[] #[] () (some $ .before op) (by sorry) (by simp) (by simp) sorry
   rewriter.replaceOp op newOp sorry sorry sorry
