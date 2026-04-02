9d526941c8122266dc594c559f22e1053a150f65
luisacicolini <lc985@cam.ac.uk>
2026-04-02 09:22:30 +0100

    fix

--- Veir/Passes/InstructionSelection/Proofs.lean
+++ Veir/Passes/InstructionSelection/Proofs.lean
@@ -5,6 +5,11 @@ import Veir.Data.Casting
 
 open Veir
 
-/-- constant in the non-poison case -/
+/--
+  Prove the correctness of the `constant` lowering pattern.
+
+  We do not need to consider the poison case, as the semantics of `llvm_constant`
+  are always concrete in the interpreter.
+-/
 theorem constant_val (v : Int) :
     (Data.LLVM.Int.val v) = RISCV.Reg.toInt (Data.RISCV.li v) 64 := by rfl
