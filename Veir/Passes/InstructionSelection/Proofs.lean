f15239ac10f34c934c646afe1452a60ecae4068b
luisacicolini <lc985@cam.ac.uk>
2026-04-02 10:06:40 +0100

    strategy

--- Veir/Passes/InstructionSelection/Proofs.lean
+++ Veir/Passes/InstructionSelection/Proofs.lean
@@ -1,6 +1,7 @@
 import Veir.Data.RISCV.Reg.Basic
 import Veir.Data.LLVM.Int.Basic
 import Veir.Data.Casting
+import Std.Tactic.BVDecide
 
 /-!
   In this file we prove the correctness of the lowering patterns used in the
@@ -15,5 +16,6 @@ open Veir
   We do not need to consider the poison case, as the semantics of `llvm_constant`
   are always concrete in the interpreter.
 -/
-theorem constant_val (_ : v = BitVec.ofInt 64 val) :
-    (Data.LLVM.Int.val v) = RISCV.Reg.toInt (Data.RISCV.li v) 64 := by rfl
+theorem constant_val (h : v = BitVec.ofInt 64 val) :
+    (Data.LLVM.Int.val v) = RISCV.Reg.toInt (Data.RISCV.li v) 64 := by
+  simp [h, Data.RISCV.li, RISCV.Reg.toInt]
