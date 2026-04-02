0a52708c81d47eb3ff92f8c8cf0c694595147acf
luisacicolini <lc985@cam.ac.uk>
2026-04-02 14:34:21 +0100

    sideeffects

--- Veir/Passes/DCE/dce.lean
+++ Veir/Passes/DCE/dce.lean
@@ -6,13 +6,18 @@ namespace Veir
 
 /-! We implement a dead code elimination pass. -/
 
+/--
+  We ensure not to delete operations like `func.func` that return nothing,
+  but wrap the entire module at a higher level.
+-/
+def hasSideEffects (rewriter: PatternRewriter OpCode) (op: OperationPtr) : Bool :=
+  if 0 < op.getNumResults! rewriter.ctx then true else false
+
 set_option warn.sorry false in
 def eliminateDeadOp (rewriter: PatternRewriter OpCode) (op: OperationPtr) :
     Option (PatternRewriter OpCode) := do
-  /- delete operations that are not used and return at least one result.
-    with the second condition we ensure not to delete operations like `func.func` that
-    return nothing but wrap the entire module at a higher level. -/
-  if ¬ op.hasUses! rewriter.ctx  && 0 < op.getNumResults! rewriter.ctx then
+  /- delete operations that are not used and have no side effects -/
+  if ¬ op.hasUses! rewriter.ctx && hasSideEffects rewriter op then
     rewriter.eraseOp op sorry
   else
     return rewriter
