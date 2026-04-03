7cef7d8166b56df4e2279df9824c91a0164ba711
luisacicolini <lc985@cam.ac.uk>
2026-04-03 10:05:13 +0100

    wjhy wont you wokr

--- Veir/Passes/CastsReconciliation/Reconciliation.lean
+++ Veir/Passes/CastsReconciliation/Reconciliation.lean
@@ -9,7 +9,7 @@ namespace Veir
   when the input and output types are the same.
 -/
 set_option warn.sorry false in
-def reconcileIdentityCast (rewriter : PatternRewriter OpCode) (op : OperationPtr) :
+def reconcilePairingCast (rewriter : PatternRewriter OpCode) (op : OperationPtr) :
     Option (PatternRewriter OpCode) := do
   let some cast := matchCastOp op rewriter.ctx | return rewriter
   let input := op.getOperand! rewriter.ctx 0
@@ -27,10 +27,22 @@ def reconcileIdentityCast (rewriter : PatternRewriter OpCode) (op : OperationPtr
   rewriter.eraseOp op sorry
 
 set_option warn.sorry false in
+def reconcileIdentityCast (rewriter : PatternRewriter OpCode) (op : OperationPtr) :
+    Option (PatternRewriter OpCode) := do
+  let some cast := matchCastOp op rewriter.ctx | return rewriter
+  /- get the input and output types -/
+  let input := op.getOperand! rewriter.ctx 0
+  let inputType := input.getType! rewriter.ctx
+  let resultType := ((op.getResult 0).get! rewriter.ctx).type
+  if inputType ≠ resultType then return rewriter
+  let rewriter ← rewriter.replaceValue (op.getResult 0) input sorry sorry
+  rewriter.eraseOp op sorry
+
+set_option warn.sorry false in
 def CastReconcilePass.impl (ctx : { ctx' : IRContext OpCode // ctx'.WellFormed }) (op : OperationPtr)
     (_ : op.InBounds ctx.val) :
     ExceptT String IO { ctx' : IRContext OpCode // ctx'.WellFormed } := do
-  let pattern := RewritePattern.GreedyRewritePattern #[reconcileIdentityCast]
+  let pattern := RewritePattern.GreedyRewritePattern #[reconcilePairingCast, reconcileIdentityCast]
   match RewritePattern.applyInContext pattern ctx ctx.property.inBounds with
   | none => throw "Error while applying cast reconciliation"
   | some ctx => pure ⟨ctx, sorry⟩
