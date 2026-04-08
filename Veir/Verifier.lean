83bbf108bc68219a82cbe866be80193c1f9a48ae
Tobias Grosser <tobias@grosser.es>
2026-04-08 21:16:37 +0100

    Test

--- Veir/Verifier.lean
+++ Veir/Verifier.lean
@@ -95,6 +95,7 @@ def OperationPtr.verifyLocalInvariants (op : OperationPtr) (ctx : IRContext OpCo
       throw "Expected 0 regions"
     if op.getNumSuccessors ctx opIn ≠ 0 then
       throw "Expected 0 successors"
+    pure ()
   | .arith .divsi => do
     if op.getNumOperands ctx opIn ≠ 2 then
       throw "Expected 2 operands"
