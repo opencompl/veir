811aa4cc39f1fa7046cae2393fe322fc4e11c9af
luisacicolini <lc985@cam.ac.uk>
2026-04-13 16:58:31 +0100

    comment

--- Veir/Data/LLVM/Int/Basic.lean
+++ Veir/Data/LLVM/Int/Basic.lean
@@ -67,6 +67,7 @@ instance {w : Nat} : ToString (Int w) where
 
 /--
   We define the semantics of a `constant` operation.
+  The result of this operation is never poison.
 -/
 def constant (w : Nat) (v : _root_.Int) : Int w := Id.run do
   val (BitVec.ofInt w v)
