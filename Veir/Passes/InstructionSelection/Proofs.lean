ab8da04cab85234bf7d18df950df447467579504
luisacicolini <lc985@cam.ac.uk>
2026-04-13 23:45:37 +0100

    addition proof

--- Veir/Passes/InstructionSelection/Proofs.lean
+++ Veir/Passes/InstructionSelection/Proofs.lean
@@ -21,3 +21,20 @@ open Classical
 theorem constant:
     IntIsRefinedByReg (Data.LLVM.Int.constant 64 v) (Data.RISCV.li (BitVec.ofInt 64 v)) := by
   simp [IntIsRefinedByReg, Data.LLVM.Int.constant, Data.RISCV.li]
+
+/--
+  Prove the correctness of the `add` lowering pattern.
+-/
+theorem add:
+    IntIsRefinedByReg (Data.LLVM.Int.add x y) (Data.RISCV.add (LLVM.Int.toReg x) (LLVM.Int.toReg y)) := by
+  simp only [IntIsRefinedByReg, Data.LLVM.Int.add, Bool.false_eq_true, false_and, reduceIte,
+    pure_bind, Data.RISCV.add, LLVM.Int.toReg, BitVec.truncate_eq_setWidth, BitVec.setWidth_eq]
+  split
+  · split
+    · split
+      <;> bv_decide
+    · split
+      · bv_decide
+      · simp only [Id.run, Data.LLVM.Int.val.injEq] at *
+        bv_decide
+  · bv_decide
