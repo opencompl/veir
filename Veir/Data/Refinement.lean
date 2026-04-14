499f308f5f074e010046f16bde1952e0c64c9b8c
luisacicolini <lc985@cam.ac.uk>
2026-04-14 09:59:21 +0100

    new def

--- Veir/Data/Refinement.lean
+++ Veir/Data/Refinement.lean
@@ -18,3 +18,17 @@ def IntIsRefinedByReg [DecidableEq Prop] (i : Veir.Data.LLVM.Int 64) (r : Veir.D
   match i with
   | .val v => r.val = v
   | .poison => true
+
+/--
+  `LLVM.Int` is refined by `LLVM.Int`.
+  `i'` refines `i` if its behaviour are a subset of the behaviours allowed by `i`.
+  In particular, any concrete `i'` refines a poison `i`, but a poison `i'` does *not* refine
+  any `i`.
+-/
+def isRefinedBy [DecidableEq Prop] (i i' : Veir.Data.LLVM.Int 64)  : Prop :=
+  match i with
+  | .val v =>
+    match i' with
+    | .val v' => v = v'
+    | .poison => true
+  | .poison => true
