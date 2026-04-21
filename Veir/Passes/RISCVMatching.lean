100e110aca7baa446cbebd43805c88ca7acf629d
luisacicolini <lc985@cam.ac.uk>
2026-04-21 11:58:52 +0100

    fix

--- Veir/Passes/RISCVMatching.lean
+++ Veir/Passes/RISCVMatching.lean
@@ -2,15 +2,9 @@ import Veir.Pass
 import Veir.PatternRewriter.Basic
 import Veir.Passes.Matching
 
-/-! This file contains helper functions to match operations when defining a rewrite. -/
+/-! This file contains helper functions to match RISCV operations. -/
 
 namespace Veir.RISCV
-
-/--
-  Match an operation that has a single result, a specific opcode,
-  and a specific number of operands.
-  Returns the operands and the properties of the operation if it matches, or `none` otherwise.
--/
 
 def matchAdd (op : OperationPtr) (ctx : IRContext OpCode) : Option (ValuePtr × ValuePtr) := do
   let (op, _) ← matchOp op ctx (.riscv .add) 2
