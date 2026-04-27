eedf9fc940d5bd4417242d1d4cc5fb6fae4bbdae
Siddharth Bhat <siddu.druid@gmail.com>
2026-04-27 09:08:50 +0100

    chore: add link to handbook of FP arithmetic textbook

--- Veir/Data/FP/PackedFloat/Basic.lean
+++ Veir/Data/FP/PackedFloat/Basic.lean
@@ -9,6 +9,10 @@ public section
 /--
 A packed floating point number,
 with exponent and significand widths encoded at the type level.
+This implements the IEEE-754 floating point specification [1] of the bit layout.
+[1] Muller, Jean-Michel, et al. Handbook of floating-point arithmetic.
+    Vol. 1. Basel, Switzerland:: Birkhäuser, 2018.
+
 -/
 @[ext]
 structure PackedFloat (exWidth sigWidth : Nat) where
