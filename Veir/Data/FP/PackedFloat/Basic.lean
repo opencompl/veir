028b711cd022c06fe3d328f9ee99232b877ea1b2
Siddharth Bhat <siddu.druid@gmail.com>
2026-04-27 09:41:01 +0100

    chore: more docs

--- Veir/Data/FP/PackedFloat/Basic.lean
+++ Veir/Data/FP/PackedFloat/Basic.lean
@@ -7,19 +7,21 @@ namespace PackedFloat
 public section
 
 /--
-A packed floating point number,
+A packed floating point number of radix 2,
 with exponent and significand widths encoded at the type level.
-This implements the IEEE-754 floating point specification [1] of the bit layout.
-[1] Muller, Jean-Michel, et al. Handbook of floating-point arithmetic.
+This definition implements Section 3.4, Figure 3.1 of the IEEE-754 standard [1].
+We recommend [2] for a comprehensive introduction.
+[1] IEEE 754-2019, https://standards.ieee.org/ieee/754/6210/
+[2] Muller, Jean-Michel, et al. Handbook of floating-point arithmetic.
     Vol. 1. Basel, Switzerland:: Birkhäuser, 2018.
-
 -/
 @[ext]
 structure PackedFloat (exWidth sigWidth : Nat) where
     /-- Sign bit. -/
     sign : Bool
-    /-- Exponent of the packed float. -/
+    /-- Biased exponent of the packed float. -/
     ex : BitVec exWidth
-    /-- Significand (mantissa) of the packed float. -/
+    /-- Trailing significand (mantissa) of the packed float.
+      Leading digit implicitly encoded by biased exponent. -/
     sig : BitVec sigWidth
 deriving DecidableEq, Repr, Inhabited
