31c6c99a9a831ee4ac7f06fdf00cfe38e46ce7b9
Siddharth <siddu.druid@gmail.com>
2026-05-04 10:19:37 +0100

    Apply suggestions from code review
    Co-authored-by: Tobias Grosser <github@grosser.es>

--- Veir/Data/FP/Sign.lean
+++ Veir/Data/FP/Sign.lean
@@ -4,7 +4,7 @@ namespace Veir.Data.FP
 
 /--
 Treat a boolean as a sign,
-where `true` corresponds to -1 and `false` corresponds to `1`.
+where `true` corresponds to `-1` and `false` corresponds to `1`.
 -/
 public def signToInt (s : Bool) : Rat :=
   if s then -1 else 1
