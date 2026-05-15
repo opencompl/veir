67dd4e5781cbaa56f93dc027c27bd138e7d781a5
John Regehr <regehr@cs.utah.edu>
2026-05-15 11:56:19 -0600

    oops, simp did not need getValue

--- Veir/Data/LLVM/Int/Bitblast.lean
+++ Veir/Data/LLVM/Int/Bitblast.lean
@@ -394,5 +394,5 @@ theorem isPoison_select {w : Nat} (x y : Int w) (c : Int 1) :
 @[llvm_toBitVec, grind =]
 theorem getValue_select {w : Nat} (x y : Int w) (c : Int 1) (h : (select c x y).isPoison = false) :
     (select c x y).getValue h = if _ : c.getValue = 1#1 then x.getValue else y.getValue := by
-  simp [select, Id.run, getValue]
+  simp [select, Id.run]
   grind
