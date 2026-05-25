38d14998170743b6e677d140f570121098e884cc
John Regehr <regehr@cs.utah.edu>
2026-05-25 08:51:23 -0600

    address Leo feedback!

--- Veir/Passes/CSE.lean
+++ Veir/Passes/CSE.lean
@@ -7,7 +7,8 @@ import Veir.Data.LLVM.Int.Basic
 
   This pass implements a small, conservative CSE:
   * it only reasons within one basic block;
-  * it only considers arithmetic operations;
+  * it only considers arithmetic operations (including icmps, select,
+    and ext/trunc);
   * it only works for instructions that return a single result;
   * distinct UB flags are treated as distinct instructions;
   * it only supports the LLVM dialect (arith would be trivial to add);
@@ -18,25 +19,24 @@ import Veir.Data.LLVM.Int.Basic
 namespace Veir
 namespace CSE
 
-/- Here we package up an opcode with its UB flags; we don't want to
+/-- Here we package up an opcode with its UB flags; we don't want to
    mix up, e.g., "add" and "add nsw". -/
 abbrev Kind := (op : OpCode) × propertiesOf op
 
 instance : Hashable Kind where
   hash k := mixHash (hash k.fst) (hash k.snd)
 
-/- This is the basis for CSE: if two instructions have the same Key,
-   then they compute the same value: they are interchangeable. In that
-   case we can remove one of them and switch all of its uses to the
-   other. Proving this will be the crux of the eventual correctness
-   proof for this pass. -/
+/-- This is the basis for CSE: if two instructions have the same Key,
+    then they compute the same value: they are interchangeable. In
+    that case we can remove one of them and switch all of its uses to
+    the other. Proving this will be the crux of the eventual
+    correctness proof for this pass. -/
 structure Key where
   kind : Kind
   resultType : TypeAttr
   operands : Array ValuePtr
 deriving DecidableEq, BEq, Hashable
 
-/- Constructor -/
 def makeKey
     (ctx : IRContext OpCode) (op : OperationPtr) (kind : Kind)
     (operands : Array ValuePtr) :
@@ -47,19 +47,19 @@ def makeKey
     operands
   }
 
-/- Because ValuePtr is a sum type where the numeric IDs assigned to an
-   opResult and a blockArgument might overlap, we add a separate tag
-   to discriminate them reliably for purposes of computing a total
-   order -/
+/-- Because ValuePtr is a sum type where the numeric IDs assigned to
+    an opResult and a blockArgument might overlap, we add a separate
+    tag to discriminate them reliably for purposes of computing a
+    total order -/
 def valueSortKey (value : ValuePtr) : Nat × Nat × Nat :=
   match value with
   | .opResult result => (0, result.op.id, result.index)
   | .blockArgument arg => (1, arg.block.id, arg.index)
 
-/- Given two ValuePtrs (that are operands to a commutative binop),
-   return them in a canonical order so that we can CSE the binop
-   regardless of the order in which its operands happen to be
-   specified -/
+/-- Given two ValuePtrs (that are operands to a commutative binop),
+    return them in a canonical order so that we can CSE the binop
+    regardless of the order in which its operands happen to be
+    specified -/
 def binaryOperandsCanonicalOrder (lhs rhs : ValuePtr) : ValuePtr × ValuePtr :=
   let (lhsTag, lhsID, lhsIndex) := valueSortKey lhs
   let (rhsTag, rhsID, rhsIndex) := valueSortKey rhs
@@ -69,7 +69,6 @@ def binaryOperandsCanonicalOrder (lhs rhs : ValuePtr) : ValuePtr × ValuePtr :=
   else
     (rhs, lhs)
 
-/- Compute the Key for a commutative binary instruction -/
 def commutativeBinopKey (ctx : IRContext OpCode) (op : OperationPtr)
     (kind : Kind) : Key :=
   let lhs := op.getOperand! ctx 0
@@ -77,15 +76,14 @@ def commutativeBinopKey (ctx : IRContext OpCode) (op : OperationPtr)
   let (lhs, rhs) := binaryOperandsCanonicalOrder lhs rhs
   makeKey ctx op kind #[lhs, rhs]
 
-/- Compute the Key for a generic instruction -/
 def ordinaryKey
     (ctx : IRContext OpCode) (op : OperationPtr) (kind : Kind) :
     Key :=
   makeKey ctx op kind (op.getOperands! ctx)
 
-/- Return op's Key if it is supported by this pass, otherwise return
-   None. TODO: It would be nice to recognize that x<y and y>x are the
-   same computation. We leave this for later. -/
+/-- Return op's Key if it is supported by this pass, otherwise return
+    None. TODO: It would be nice to recognize that x<y and y>x are the
+    same computation. We leave this for later. -/
 def key? (ctx : IRContext OpCode) (op : OperationPtr) : Option Key := do
   guard ((op.get! ctx).attrs.entries.size = 0)
   guard (op.getNumResults! ctx = 1)
@@ -109,9 +107,9 @@ def key? (ctx : IRContext OpCode) (op : OperationPtr) : Option Key := do
       return ordinaryKey ctx op kind
   | _ => none
 
-/- Perform CSE on a single BB: Walk the operations, building up a list
-   of available values. For any operation whose value is already
-   available, replace it with the earlier one. -/
+/-- Perform CSE on a single BB: Walk the operations, building up a
+    hash of available values. For any operation whose value is already
+    available, replace it with the earlier one. -/
 set_option warn.sorry false in
 def processBlock
     (ctx : WfIRContext OpCode) (block : BlockPtr) :
@@ -137,8 +135,7 @@ def processAllBlocks (ctx : WfIRContext OpCode) :
     WfIRContext OpCode := Id.run do
   let mut ctx := ctx
   for block in ctx.raw.blocks.keys do
-    if block.InBounds ctx.raw then
-      ctx := processBlock ctx block
+    ctx := processBlock ctx block
   return ctx
 
 def CSEPass.impl (ctx : WfIRContext OpCode) (_op : OperationPtr) (_ : _op.InBounds ctx.raw) :
