a62ae4f1bb028e1cb08a4ac17bb716d34af2df51
Luisa Cicolini <48860705+luisacicolini@users.noreply.github.com>
2026-06-09 00:04:40 +0100

    feat(RISCV): proof strategy and tactic for isel patterns (#786)
    We introduce a new `refine_bv_decide` tactic to automate the
    verification of instruction selection patterns' refinement relation. To
    do this, we needed some additional lemmas in our simp-sets, in
    particular regarding the unfolding of the casting ops (`toInt`,
    `toReg`).
    
    We show how the tactic works in golfing the current instruction
    selection proofs.
    
    As a next step, I shall use the same strategy to prove the refinement
    relation for all the instruction selection patterns.

--- Veir/Passes/InstructionSelection/Proofs.lean
+++ Veir/Passes/InstructionSelection/Proofs.lean
@@ -1,5 +1,9 @@
 import Veir.Data.RISCV.Reg.Basic
+import Veir.Data.RISCV.Reg.Lemmas
 import Veir.Data.LLVM.Int.Basic
+import Veir.Data.LLVM.Int.Simp
+import Veir.Data.LLVM.Int.Lemmas
+import Veir.Data.LLVM.Int.Bitblast
 import Veir.Data.Casting
 import Veir.Data.Refinement
 import Std.Tactic.BVDecide
@@ -12,29 +16,28 @@ import Std.Tactic.BVDecide
 namespace Veir.Data.RISCV
 
 /--
+  A tactic to unfold the semantics of operations on `LLVM.Int` and on `RISCV.Reg` to
+  bitvectors, such that `bv_decide` can prove the correctness of the refinement relation
+  over instruction selection patterns.
+-/
+macro "refine_bv_decide" : tactic =>
+  `(tactic| ((try simp only [llvm_toBitVec, reg_toBitVec]; try simp [LLVM.Int.getValue_eq_getValueD]; try bv_decide)))
+
+
+/--
   Prove the correctness of the `constant` lowering pattern.
 
   We do not need to consider the poison case, as the semantics of `llvm_constant`
   are always concrete in the interpreter.
 -/
-theorem constant_refinement:
-    isRefinedBy (LLVM.Int.constant 64 v) (RISCV.Reg.toInt (Data.RISCV.li (BitVec.ofInt 64 v)) 64) := by
-  simp [isRefinedBy, Data.LLVM.Int.constant, Data.RISCV.li, RISCV.Reg.toInt]
+theorem constant_refinement {v : Int}:
+    (LLVM.Int.constant 64 v) ⊒ (RISCV.Reg.toInt (Data.RISCV.li (BitVec.ofInt 64 v)) 64) := by
+  refine_bv_decide
 
 /--
   Prove the correctness of the `add` lowering pattern.
 -/
-theorem add_refinement:
-    isRefinedBy (Data.LLVM.Int.add x y) (RISCV.Reg.toInt (Data.RISCV.add (LLVM.Int.toReg x) (LLVM.Int.toReg y)) 64) := by
-  simp only [isRefinedBy, Data.LLVM.Int.add, Bool.false_eq_true, false_and, ↓reduceIte,
-    pure_bind, RISCV.Reg.toInt, Data.RISCV.add, LLVM.Int.toReg, BitVec.truncate_eq_setWidth,
-    BitVec.setWidth_eq]
-  split
-  · split
-    · split
-      <;> bv_decide
-    · split
-      · bv_decide
-      · simp only [Id.run, Data.LLVM.Int.val.injEq] at *
-        bv_decide
-  · bv_decide
+theorem add_refinement {x y : LLVM.Int 64} :
+    (Data.LLVM.Int.add x y) ⊒
+      (RISCV.Reg.toInt (Data.RISCV.add (LLVM.Int.toReg x) (LLVM.Int.toReg y)) 64) := by
+  refine_bv_decide
