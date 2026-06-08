65e07941d52f822033c7efe0b831e7a267cb779a
luisacicolini <lc985@cam.ac.uk>
2026-06-08 17:36:35 +0100

    proof

--- Veir/Data/Casting.lean
+++ Veir/Data/Casting.lean
@@ -1,7 +1,10 @@
 module
 
 public import Veir.Data.LLVM.Int.Basic
+public import Veir.Data.LLVM.Int.Bitblast
 public import Veir.Data.RISCV.Reg.Basic
+public import Veir.Data.RISCV.Reg.Simp
+import all Veir.Data.LLVM.Int.Bitblast
 
 public section
 
@@ -14,6 +17,7 @@ public section
 /--
   Cast `LLVM.Int` to `RISCV.Reg`.
 -/
+@[grind]
 def LLVM.Int.toReg (i : Veir.Data.LLVM.Int w) : Veir.Data.RISCV.Reg :=
   match i with
   | .poison => .mk 0#64
@@ -22,5 +26,24 @@ def LLVM.Int.toReg (i : Veir.Data.LLVM.Int w) : Veir.Data.RISCV.Reg :=
 /--
   Cast `RISCV.Reg` to `LLVM.Int`.
 -/
+@[grind]
 def RISCV.Reg.toInt (r : Veir.Data.RISCV.Reg) (w : Nat) : Veir.Data.LLVM.Int w :=
   Veir.Data.LLVM.Int.val (BitVec.zeroExtend w r.val)
+
+@[llvm_toBitVec, grind =]
+theorem toInt_eq_val {r : Veir.Data.RISCV.Reg} :
+    (RISCV.Reg.toInt r 64).getValue = r.val := by
+  simp [RISCV.Reg.toInt, Veir.Data.LLVM.Int.getValue]
+
+@[llvm_toBitVec, grind =]
+theorem toInt_isPoison {r : Veir.Data.RISCV.Reg} :
+    (RISCV.Reg.toInt r w).isPoison = false := by
+  simp [RISCV.Reg.toInt, Veir.Data.LLVM.Int.isPoison]
+
+@[llvm_toBitVec, grind =]
+theorem val_toReg{i : Veir.Data.LLVM.Int w} :
+    (LLVM.Int.toReg i).val = if h : i.isPoison = true then 0#64 else i.getValue.zeroExtend 64 := by
+  simp only [LLVM.Int.toReg, BitVec.truncate_eq_setWidth]
+  split
+  · simp [Veir.Data.LLVM.Int.isPoison]
+  · simp [llvm_toBitVec]
--- Veir/Passes/InstructionSelection/Proofs.lean
+++ Veir/Passes/InstructionSelection/Proofs.lean
@@ -1,5 +1,9 @@
 import Veir.Data.RISCV.Reg.Basic
+import Veir.Data.RISCV.Reg.Lemmas
 import Veir.Data.LLVM.Int.Basic
+import Veir.Data.LLVM.Int.Simp
+import Veir.Data.LLVM.Int.Lemmas
+import Veir.Data.LLVM.Int.Bitblast
 import Veir.Data.Casting
 import Veir.Data.Refinement
 import Std.Tactic.BVDecide
@@ -17,24 +21,16 @@ namespace Veir.Data.RISCV
   We do not need to consider the poison case, as the semantics of `llvm_constant`
   are always concrete in the interpreter.
 -/
-theorem constant_refinement:
-    isRefinedBy (LLVM.Int.constant 64 v) (RISCV.Reg.toInt (Data.RISCV.li (BitVec.ofInt 64 v)) 64) := by
-  simp [isRefinedBy, Data.LLVM.Int.constant, Data.RISCV.li, RISCV.Reg.toInt]
+theorem constant_refinement {v : Int}:
+    (LLVM.Int.constant 64 v) ⊒ (RISCV.Reg.toInt (Data.RISCV.li (BitVec.ofInt 64 v)) 64) := by
+  simp [llvm_toBitVec, reg_toBitVec]
 
 /--
   Prove the correctness of the `add` lowering pattern.
 -/
-theorem add_refinement:
-    isRefinedBy (Data.LLVM.Int.add x y) (RISCV.Reg.toInt (Data.RISCV.add (LLVM.Int.toReg x) (LLVM.Int.toReg y)) 64) := by
-  simp only [isRefinedBy, Data.LLVM.Int.add, Bool.false_eq_true, false_and, ↓reduceIte,
-    pure_bind, RISCV.Reg.toInt, Data.RISCV.add, LLVM.Int.toReg, BitVec.truncate_eq_setWidth,
-    BitVec.setWidth_eq]
-  split
-  · split
-    · split
-      <;> bv_decide
-    · split
-      · bv_decide
-      · simp only [Id.run, Data.LLVM.Int.val.injEq] at *
-        bv_decide
-  · bv_decide
+theorem add_refinement {x y : LLVM.Int 64} :
+    (Data.LLVM.Int.add x y) ⊒
+      (RISCV.Reg.toInt (Data.RISCV.add (LLVM.Int.toReg x) (LLVM.Int.toReg y)) 64) := by
+  simp only [llvm_toBitVec, reg_toBitVec]
+  simp [LLVM.Int.getValue_eq_getValueD]
+  bv_decide
