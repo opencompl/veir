6094b8edd982fd529839c8a9e88ee43c00ba4a10
Tobias Grosser <tobias@grosser.es>
2026-06-11 00:43:52 +0100

    Clean som things up

--- Veir/Passes/InstructionSelection/RISCV64Branches.lean
+++ Veir/Passes/InstructionSelection/RISCV64Branches.lean
@@ -10,32 +10,30 @@ import Std
 
 namespace Veir
 
-
+set_option warn.sorry false in
 def convertBlock (ctx : WfIRContext OpCode) (block : BlockPtr) : ExceptT String IO (WfIRContext OpCode) := do
   let argCount := block.getNumArguments! ctx.raw
   let mut c := ctx.raw
+
+  -- If the block has no uses (e.g., the entry block) we can skip it.
   if (block.get! c).firstUse == none then
-    -- If the block has no uses (e.g., the entry block) we can skip it.
     return ⟨c, by sorry⟩
 
   let mut currentPredUse := (block.get! c).firstUse
-
   while let some block := currentPredUse do
     have op := (block.get! ctx.raw).owner
     currentPredUse := (block.get! ctx.raw).nextUse
 
-    -- Check if the terminator operations can be converted to RISCV branches. If not,
-    -- we skip converting this predecessor block.
+    -- Check if the terminator operations can be converted to RISCV branches. If
+    -- not, we exit early and do not convert this predecessor block.
     if op.getOpType! ctx.raw != .llvm .br && op.getOpType! ctx.raw != .llvm .cond_br then
       continue
 
     -- TODO: it seems we have some dangling operations which we need to ignore to
     -- make things work. Eventually, these should be removed such that they are not
     -- even dangling.
-    if (op.get! ctx.raw).parent = none then
-      continue
+    let some block := (op.get! c).parent | continue
 
-    let some block := (op.get! ctx.raw).parent | return ⟨c, by sorry⟩
     let mut ip := InsertPoint.after op ctx.raw block sorry sorry
     let mut casts : Array (OperationPtr) := #[]
 
@@ -49,15 +47,12 @@ def convertBlock (ctx : WfIRContext OpCode) (block : BlockPtr) : ExceptT String 
     if h : op.getOpType! ctx.raw = .llvm .br then do
       Rewriter.createOp c (.riscv_cf .branch) #[] (casts.map (fun cast => cast.getResult 0)) #[op.getSuccessor c 0 sorry sorry] #[] default ip sorry sorry sorry sorry sorry
     else if h : op.getOpType! ctx.raw = .llvm .cond_br then do
-      let pp : CondBrProperties := op.getProperties! c (.llvm .cond_br)
-
-      let x := pp.operandSegmentSizes
-
-      let pp : RISCVBrProperties := ⟨x⟩
-
-      Rewriter.createOp c (.riscv_cf .cbr) #[] (casts.map (fun cast => cast.getResult 0)) (op.getSuccessors c sorry) #[] (pp) ip sorry sorry sorry sorry sorry
+      let condProps : CondBrProperties := op.getProperties! c (.llvm .cond_br)
+      let props : RISCVBrProperties := ⟨condProps.operandSegmentSizes⟩
+      Rewriter.createOp c (.riscv_cf .cbr) #[] (casts.map (fun cast => cast.getResult 0)) (op.getSuccessors c sorry) #[] props ip sorry sorry sorry sorry sorry
     else
       none | return ⟨c, by sorry⟩
+
     c := Rewriter.detachOp xc op sorry sorry sorry
 
   for i in List.range (block.getNumArguments! c) do
@@ -66,14 +61,12 @@ def convertBlock (ctx : WfIRContext OpCode) (block : BlockPtr) : ExceptT String 
     c := Rewriter.setType c bap (RegisterType.mk) sorry
     let ip := InsertPoint.atStart block c sorry
     let some (xc, cast) := Rewriter.createEmptyOp c (.builtin .unrealized_conversion_cast) default | return ⟨c, by sorry⟩
-    let vp : ValuePtr := cast.getResult 0
     let some xc := Rewriter.insertOp? xc cast ip sorry sorry sorry | return ⟨c, by sorry⟩
     c := Rewriter.pushResult xc cast (IntegerType.mk 64) sorry
-
-    let some (xc) := Rewriter.replaceValue? c bap vp sorry sorry sorry | return ⟨c, by sorry⟩
+    let some (xc) := Rewriter.replaceValue? c bap (cast.getResult 0) sorry sorry sorry | return ⟨c, by sorry⟩
     c := xc
-
     c := Rewriter.pushOperand c cast bap sorry sorry sorry
+
   return ⟨c, by sorry⟩
 
 def convertModule (ctx : WfIRContext OpCode) :
