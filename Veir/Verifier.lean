ec42b4e762ddee5d43066eb54d4d4d8aefe0b012
luisacicolini <lc985@cam.ac.uk>
2026-06-11 17:30:21 +0100

    address comments

--- Veir/Verifier.lean
+++ Veir/Verifier.lean
@@ -164,9 +164,9 @@ def OperationPtr.verifyIntegerExtTypes (op : OperationPtr) (ctx : WfIRContext Op
 
 def OperationPtr.verifyRISCVneg (op : OperationPtr) (ctx : WfIRContext OpCode)
     (opIn : op.InBounds ctx.raw) (imm : Int) : Except String PUnit :=
-  if 0 ≤ imm ∧ 1048575 < imm then
+  if imm < 0 ∨ 1048575 < imm then -- 1048575 = 2 ^ 20 - 1
     let instrName := String.fromUTF8! (op.getOpType ctx.raw opIn).name
-    throw s!"{instrName} immediate out of bounds: must fit in a signed 20-bit field."
+    throw s!"{instrName} immediate out of bounds: must fit in a unsigned 20-bit field."
   else
     pure ()
 
