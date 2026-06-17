67f5410f9a357a773b4c2ee118637b04e05a2ab0
Alex Keizer <alex@keizer.dev>
2026-06-17 00:20:33 +0000

    refactor: promote `llvm_bv_decide` to a non-test module. (#878)
    This PR moves `llvm_bv_decide` from a test file into `LLVM.Int.Simp`,
    which seemed appropriate given the relevant simpset was defined there.
    
    Unfortunately, the `getValue_eq_getValueD` lemma isn't yet available in
    this file, so I introduce a new `llvm_toBitVec_post` lemma for the
    second `simp`. This extra simpset will be useful when we wish to tighten
    the simp-step. It currently includes the default global simpset, and
    indeed some of the tests break if we tighten the simp call to *just* the
    `getValueD` lemma. In a future PR, we should figure out which
    simp-lemmas are actually needed for these tests, add them to
    `llvm_toBitVec_post` and then tighten the `simp` (having a dependency on
    the default simpset seems undesirable).
    
    Futhermore, I split the tactic into a `llvm_bv_normalize` tactic which
    does just the simping, and a `llvm_bv_decide` tactic which normalizes
    (using `llvm_bv_normalize`) and then calls `bv_decide`, for easier
    debugging.
    
    Finally, I removed usages of `try` from the tactic. `try` silently
    swallows errors, making debugging harder. Instead, I use `simp
    -failIfUnchanged` to not throw an error when simp didn't change the
    goal, and `<;>` to not throw an error if a goal was solved by an earlier
    tactic in the sequence.

---
+++ Veir/Meta/BVDecide.lean
@@ -0,0 +1,39 @@
+module
+
+public import Lean
+
+/-! ## Simpsets -/
+
+register_simp_attr veir_bv_normalize
+register_simp_attr veir_bv_normalize_post
+
+/-! ## Veir Bitblasting Tactic -/
+
+/--
+`veir_bv_normalize` preprocesses a goal with `LLVM.Int`s and other bitblastable
+Veir types into a form suitable for bitblasting, using the `veir_bv_normalize`
+simpset followed by the `veir_bv_normalize_post` simpset.
+
+Note: Preprocessing lemmas should almost always be added to `veir_bv_normalize`.
+Preprocessing happens in two stages because rewrites are generally
+easier to state with the dependently-typed `Int.getValue`, but `bv_decide` works
+better with the non-dependent `Int.getValueD`. Thus, `veir_bv_normalize_post`
+contains `Int.getValue_eq_getValueD`, which rewrites the former into the latter.
+-/
+@[expose] macro "veir_bv_normalize" : tactic =>
+  `(tactic| ((
+      simp -failIfUnchanged only [veir_bv_normalize] <;>
+      simp -failIfUnchanged [veir_bv_normalize_post, -BitVec.extractLsb_toNat])))
+/- TODO: Tighten the last `simp` into a `simp only`.
+The lack of `only` is copied from the previous version of this tactic, and indeed
+some of the tests seem to rely on certain simp-lemmas from the default simp-set.
+We should figure out which ones, and add just those to `veir_bv_normalize_post`.
+-/
+
+/--
+`veir_bv_decide` preprocesses a goal with `LLVM.Int`s and other bitblastable
+Veir types into a form suitable for bitblasting, via `veir_bv_normalize`, and
+then calls `bv_decide` to automatically close the goal.
+-/
+@[expose] macro "veir_bv_decide" : tactic =>
+  `(tactic| ((veir_bv_normalize <;> bv_decide)))
