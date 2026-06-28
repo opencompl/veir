87ad7bda0818e3ca4a4d80a6a9e04d50e1a8ccb0
John Regehr <regehr@cs.utah.edu>
2026-06-28 08:33:35 -0600

    tiny panic utility

---
+++ Veir/Panic.lean
@@ -0,0 +1,9 @@
+namespace Veir
+
+@[extern "lean_internal_set_exit_on_panic"]
+opaque setExitOnPanic (exit : Bool) : BaseIO Unit
+
+def enableExitOnPanic : BaseIO Unit :=
+  setExitOnPanic true
+
+end Veir
