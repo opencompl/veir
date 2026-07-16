fd10491ea9506b1f3363036f188d1d74597c04ce
Mathieu Fehr <mathieu.fehr@gmail.com>
2026-07-16 03:01:25 +0100

    feat(verifier): Add check that successors have the same parents

--- UnitTest.lean
+++ UnitTest.lean
@@ -4,6 +4,7 @@ import UnitTest.Parser
 import UnitTest.AttrParser
 import UnitTest.MlirParser
 import UnitTest.IR.Operation
+import UnitTest.Verifier
 import UnitTest.FP
 import UnitTest.Bitblasting.Bitblasting
 import UnitTest.DataFlowFramework.Dominance
