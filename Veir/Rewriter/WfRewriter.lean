da24651210fb7ce3cc8f77288dcc3307ee29e7e0
John Regehr <regehr@cs.utah.edu>
2026-07-27 14:56:32 -0600

    move theorems as suggested by mathieu

--- Veir/Rewriter/WfRewriter.lean
+++ Veir/Rewriter/WfRewriter.lean
@@ -3,3 +3,4 @@ module
 public import Veir.Rewriter.WfRewriter.Basic
 public import Veir.Rewriter.WfRewriter.GetSet
 public import Veir.Rewriter.WfRewriter.InBounds
+public import Veir.Rewriter.WfRewriter.Lemmas
