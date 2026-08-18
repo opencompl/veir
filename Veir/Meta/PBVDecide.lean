8b26566c3a0ffa55016915f2379ea8007f7edb49
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-08-18 18:40:36 +0100

    working first application of the width_elim_theorem

--- Veir/Meta/PBVDecide.lean
+++ Veir/Meta/PBVDecide.lean
@@ -12,16 +12,18 @@ def pbvTranslate (g : MVarId) (bound : Nat) : TacticM MVarId := do
   logInfo ("Deciding with bound")
   let width_elim_theorem ← mkConstWithFreshMVarLevels ``width_elim
   let out ← g.withContext do
-    let mut width_vars : Array FVarId := #[]
     for ldecl in ← getLCtx do
       unless ldecl.isImplementationDetail do
         if ← isDefEq ldecl.type (mkConst ``Nat) then
           logInfo m!"Found a var! {ldecl.userName}"
-          width_vars := width_vars.push ldecl.fvarId
-    return width_vars
+          let applied := mkAppN width_elim_theorem #[mkNatLit bound, ldecl.toExpr, ← g.getType]
+          return ← g.apply applied
+    throwError "haven't thought about this yet"
+
+  let some new_g := out[0]? | throwError "Shuold have a goal here"
 
   logInfo (toString width_elim_theorem)
-  return g
+  return new_g
 
 
 syntax (name := pbvDecide) "pbv_decide" optConfig (ppSpace colGt num)? : tactic
