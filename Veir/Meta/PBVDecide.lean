96b1a5663d28e970ab864d4898b392bd9bd6f62d
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-08-18 17:59:35 +0100

    something is happening

---
+++ Veir/Meta/PBVDecide.lean
@@ -0,0 +1,38 @@
+import Lean
+import Veir.Data.PBV
+
+open Lean Elab Tactic Meta
+
+namespace Veir.Data.PBV
+
+-- Start by only trying to apply width elim, and generating some new hyps
+-- The width_elim has type:  (o w : Nat) (Q : Prop) (h : ∀ (m : BitVec o), m = maskOfWidth o w → Q) : Q
+-- so, will "feed" the current o w and goal (Q) and it will generate a new hole with the hypothesis and the Q
+def pbvTranslate (g : MVarId) (bound : Nat) : TacticM MVarId := do
+  logInfo ("Deciding with bound")
+  let width_elim_theorem ← mkConstWithFreshMVarLevels ``width_elim
+  let out ← g.withContext do
+    let mut width_vars : Array FVarId := #[]
+    for ldecl in ← getLCtx do
+      unless ldecl.isImplementationDetail do
+        if ← isDefEq ldecl.type (mkConst ``Nat) then
+          logInfo m!"Found a var! {ldecl.userName}"
+          width_vars := width_vars.push ldecl.fvarId
+    return width_vars
+
+  logInfo (toString width_elim_theorem)
+  return g
+
+
+syntax (name := pbvDecide) "pbv_decide" optConfig (ppSpace colGt num)? : tactic
+
+@[tactic pbvDecide]
+def evalPbvDecide : Tactic := fun stx => do
+  let out ← pbvTranslate (← getMainGoal) stx[0].toNat
+  replaceMainGoal [out]
+
+
+
+theorem trace_add_comm (w : Nat) (x y : BitVec w) (hw : w ≤ 4) :
+  x + y = y + x := by
+  pbv_decide 9
