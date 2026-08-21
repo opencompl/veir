dcaabc6b9d548a3e9018696677386648095e00f3
luigirinaldi <luigirinaldi@users.noreply.github.com>
2026-08-21 11:34:52 +0100

    chore: add some docs and examples

--- UnitTest/BoundedBitblasting/BoundedBitblasting.lean
+++ UnitTest/BoundedBitblasting/BoundedBitblasting.lean
@@ -5,3 +5,15 @@ example (w : Nat) (x y : BitVec w) (hw : w ≤ 4) :
   pbv_decide 13
   · bv_decide
   · grind
+
+example (w : Nat) (x y z : BitVec w) (hw : w ≤ 4) :
+  x + y + z = y + x + z := by
+  pbv_decide 13
+  · bv_decide
+  · grind
+
+example (w : Nat) (x : BitVec (w + 0)) (y : BitVec w) (hw : w ≤ 4) :
+  x + y = y + x := by
+  pbv_decide 13
+  · bv_decide
+  · grind
