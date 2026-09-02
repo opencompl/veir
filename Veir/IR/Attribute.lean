75949dc82bf15382ae2ee26ea7ef2b67308db40c
Maschmalow <maschmalow@gmail.com>
2026-09-02 11:29:22 +0100

    style: apply suggestions from review

--- Veir/IR/Attribute.lean
+++ Veir/IR/Attribute.lean
@@ -1664,20 +1664,14 @@ instance : IsTypeAttr PDL.TypeType where
   coe_eq_inject _ := by rfl
 
 instance : IsTypeAttr TypeAttr where
-  name:= "TypeAttr"
+  name := "TypeAttr"
   inject := fun x => x.val
-  project := fun attr => if h:attr.isType then attr.asType else none
+  project := fun attr => if h: attr.isType then attr.asType else none
   coe := id
   coe_eq_inject := by simp
-  project_eq_some_iff x x' := by
-    simp
-    constructor
-    · intros q
-      grind
-    · intros q
-      exists q ▸ x'.property
-      simp [← q]
-      rfl
+  project_eq_some_iff _ _ := by
+    simp only [Option.dite_none_right_eq_some, Option.some.injEq, TypeAttr.inj]
+    grind
 
 end
 end Veir
