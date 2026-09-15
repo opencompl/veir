04eb0f1834b2a8be3107a10e1b2a7f0163cc8dc5
Naveen Seth Hanig <naveen.hanig@outlook.com>
2026-09-15 14:19:31 +0100

    Apply suggestion
    Co-authored-by: Luigi Rinaldi <82770895+luigirinaldi@users.noreply.github.com>

--- Veir/Passes/Legalization/Proofs.lean
+++ Veir/Passes/Legalization/Proofs.lean
@@ -25,11 +25,10 @@ theorem add_widening (w t : Nat) (i i' : LLVM.Int w) (ext ext' : BitVec (t - w))
           (LLVM.Int.ext i' t ext' hIsWiden) false false)
         w false false hIsWiden := by
   veir_bv_normalize
-  constructor
-  · simp
-  · intros
-    pbv_decide 16
-    · bv_decide
+  refine ⟨by simp, fun _ _ => ?_⟩
+  intros
+  pbv_decide 16
+  bv_decide
 
 end Veir.Data.LLVM
 
