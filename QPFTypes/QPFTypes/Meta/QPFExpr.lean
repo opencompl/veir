2b392ce8532d482271969b7cbfcec126cdcb57b9
Alex Keizer <alex@keizer.dev>
2026-09-18 17:05:52 +0100

    feat (QPFTypes): QPFExpr meta helper
    This PR introduces the `QPFExpr` structure, which contains a Lean expression of a multi-variate type functor, together with the Lean expression of the instance showing it is a QPF. This will be usefull to manipulate these type functors in meta-code; this structure is morally a specialization of QQ'f `Q(...)` type.
    
    Initial helpers for constructing the (co)fixpoint of a QPF are provided, although further helpers are currently blocked on more theory that needs to be ported from Mathlib.

---
+++ QPFTypes/QPFTypes/Meta/QPFExpr.lean
@@ -0,0 +1,11 @@
+module
+
+public import QPFTypes.Meta.QPFExpr.Basic
+
+/-!
+# QPFExpr
+
+Main definitions:
+  * `QPFExpr u n`, the type of Lean expressions of an `n`-ary type function,
+    with correponding QPF instances.
+-/
